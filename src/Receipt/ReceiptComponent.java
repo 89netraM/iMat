fc6a65496b29e43552fcae5f7922a4b15d13482c
Mårten Åsberg <marten.asberg@outlook.com>
2019-05-04 22:30:27 +0200

    Alla UI delar (utan stil)
    Lägger till alla UI delar, men väntar med att styla dem tills senare.

--- src/Receipt/ReceiptComponent.java
+++ src/Receipt/ReceiptComponent.java
@@ -1,7 +1,10 @@
 package Receipt;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import se.chalmers.cse.dat216.project.CartEvent;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
@@ -12,7 +15,22 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ReceiptComponent extends VBox {
+public class ReceiptComponent extends GridPane {
+	@FXML
+	private VBox receiptList;
+
+	@FXML
+	private GridPane undoPane;
+	@FXML
+	private Label undoText;
+	@FXML
+	private Button undoButton;
+
+	@FXML
+	private Button backButton;
+	@FXML
+	private Button checkoutButton;
+
 	private final ShoppingCart cart;
 	private final Map<Integer, Label> receiptItems = new HashMap<>();
 
@@ -40,7 +58,7 @@ public class ReceiptComponent extends VBox {
 			Label label = new Label(product.getName() + ": " + e.getShoppingItem().getAmount());
 
 			receiptItems.put(product.getProductId(), label);
-			getChildren().add(label);
+			receiptList.getChildren().add(label);
 		}
 		else {
 			if (e.getShoppingItem().getAmount() > 0.0d) {
@@ -48,8 +66,22 @@ public class ReceiptComponent extends VBox {
 				receiptItems.get(product.getProductId()).setText(product.getName() + ": " + e.getShoppingItem().getAmount());
 			}
 			else {
-				getChildren().remove(receiptItems.remove(product.getProductId()));
+				receiptList.getChildren().remove(receiptItems.remove(product.getProductId()));
 			}
 		}
+	}
+
+	@FXML
+	private void onUndoButton() {
+
+	}
+
+	@FXML
+	private void onBackButton() {
+
+	}
+	@FXML
+	private void onCheckoutButton() {
+
 	}
 }
