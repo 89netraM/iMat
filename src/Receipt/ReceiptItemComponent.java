f778e0292c48818633ce3cb8a37882ba2fe1b54e
Mårten Åsberg <marten.asberg@outlook.com>
2019-05-05 09:14:57 +0200

    Uppdatera item med titel och total kostnad

--- src/Receipt/ReceiptItemComponent.java
+++ src/Receipt/ReceiptItemComponent.java
@@ -1,11 +1,24 @@
 package Receipt;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import se.chalmers.cse.dat216.project.CartEvent;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.io.IOException;
 
 public class ReceiptItemComponent extends GridPane {
+	@FXML
+	private Label nameLabel;
+
+	@FXML
+	private Label totalLabel;
+
+	private ShoppingItem item;
+
 	public ReceiptItemComponent() {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ReceiptItemComponent.fxml"));
 		fxmlLoader.setRoot(this);
@@ -17,5 +30,26 @@ public class ReceiptItemComponent extends GridPane {
 		catch (IOException ex) {
 			throw new RuntimeException(ex);
 		}
+
+		IMatDataHandler.getInstance().getShoppingCart().addShoppingCartListener(this::onCartEvent);
+	}
+
+	private void onCartEvent(CartEvent e) {
+		if (e.getShoppingItem() == item) {
+			updateUI();
+		}
+	}
+
+	private void updateUI() {
+		nameLabel.setText(item.getProduct().getName());
+		totalLabel.setText(item.getTotal() + " kr");
+	}
+
+	public void setItem(ShoppingItem value) {
+		item = value;
+		updateUI();
+	}
+	public ShoppingItem getItem() {
+		return item;
 	}
 }
