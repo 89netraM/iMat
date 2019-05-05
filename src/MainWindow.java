50b7d3803ec63d0d6a430be5a33c86bb7f45d4d6
Mårten Åsberg <marten.asberg@outlook.com>
2019-05-05 18:46:16 +0200

    Räknaren lyssnar på ändringar

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,3 +1,42 @@
-public class MainWindow {
+import ProductCounter.ProductCounterComponent;
+import javafx.fxml.FXML;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.ShoppingCart;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
+public class MainWindow {
+	@FXML
+	private ProductCounterComponent counter;
+
+	private ShoppingItem item;
+
+	private ShoppingCart cart;
+
+	public MainWindow() {
+		IMatDataHandler IMat = IMatDataHandler.getInstance();
+		cart = IMat.getShoppingCart();
+		cart.addProduct(IMat.getProduct(1));
+		item = cart.getItems().get(0);
+	}
+
+	@FXML
+	private void plus() {
+		change(1);
+	}
+	@FXML
+	private void minus() {
+		change(-1);
+	}
+
+	private void change(double change) {
+		item.setAmount(item.getAmount() + change);
+		cart.fireShoppingCartChanged(item, false);
+	}
+
+	@FXML
+	private void load() {
+		if (counter != null) {
+			counter.setShoppingItem(item);
+		}
+	}
 }
--- src/ProductCounter/ProductCounterComponent.java
+++ src/ProductCounter/ProductCounterComponent.java
@@ -1,11 +1,22 @@
 package ProductCounter;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import se.chalmers.cse.dat216.project.CartEvent;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.io.IOException;
 
 public class ProductCounterComponent extends HBox {
+	@FXML
+	private TextField amountTextField;
+
+	private ShoppingItem item;
+	private double amount = 0;
+
 	public ProductCounterComponent() {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ProductCounterComponent.fxml"));
 		fxmlLoader.setRoot(this);
@@ -17,5 +28,28 @@ public class ProductCounterComponent extends HBox {
 		catch (IOException ex) {
 			throw new RuntimeException(ex);
 		}
+
+		IMatDataHandler.getInstance().getShoppingCart().addShoppingCartListener(this::onCartEvent);
+	}
+
+	public void setShoppingItem(ShoppingItem value) {
+		item = value;
+		updateUIAmount();
+	}
+	public ShoppingItem getShoppingItem() {
+		return item;
+	}
+
+	private void onCartEvent(CartEvent e) {
+		if (e.getShoppingItem() == item) {
+			if (item.getAmount() != amount) {
+				updateUIAmount();
+			}
+		}
+	}
+
+	private void updateUIAmount() {
+		amount = item.getAmount();
+		amountTextField.setText(Double.toString(amount));
 	}
 }
