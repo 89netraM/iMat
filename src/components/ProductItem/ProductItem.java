1a0ddaaeca033bae5ed537ff21cf8e32a6774789
Robin Hallabro-Kokko <robin@hallabro.nu>
2019-05-05 17:59:33 +0200

    Initial commit

---
+++ src/components/ProductItem/ProductItem.java
@@ -0,0 +1,13 @@
+package components.ProductItem;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ProductItem {
+    @FXML
+    private Label description;
+
+    @FXML
+    private ImageView image;
+}
