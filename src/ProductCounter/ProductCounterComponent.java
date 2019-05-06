8a9a376f71022092da21280fff9c271774dba7de
Mårten Åsberg <marten.asberg@outlook.com>
2019-05-06 14:31:17 +0200

    Enhet syns i text rutan

--- src/ProductCounter/ProductCounterComponent.java
+++ src/ProductCounter/ProductCounterComponent.java
@@ -1,10 +1,15 @@
 package ProductCounter;
 
+import com.sun.javafx.tk.Toolkit;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.HBox;
+import javafx.util.converter.DoubleStringConverter;
 import se.chalmers.cse.dat216.project.CartEvent;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.ShoppingCart;
@@ -15,6 +20,8 @@ import java.io.IOException;
 public class ProductCounterComponent extends HBox {
 	@FXML
 	private TextField amountTextField;
+	@FXML
+	private Label unitLabel;
 
 	private ShoppingCart cart;
 	private ShoppingItem item;
@@ -35,6 +42,7 @@ public class ProductCounterComponent extends HBox {
 		cart.addShoppingCartListener(this::onCartEvent);
 
 		amountTextField.focusedProperty().addListener(this::onFocusChange);
+		amountTextField.setTextFormatter(new TextFormatter<>(new DoubleStringConverter()));
 	}
 
 	public void setShoppingItem(ShoppingItem value) {
@@ -69,6 +77,12 @@ public class ProductCounterComponent extends HBox {
 
 	private void updateUIAmount() {
 		amountTextField.setText(Double.toString(item.getAmount()));
+
+		if (!item.getProduct().getUnitSuffix().equals(unitLabel.getText())) {
+			unitLabel.setText(item.getProduct().getUnitSuffix());
+			double labelWidth = Toolkit.getToolkit().getFontLoader().computeStringWidth(unitLabel.getText(), unitLabel.getFont());
+			amountTextField.setPadding(new Insets(0, unitLabel.getPadding().getLeft() + labelWidth + unitLabel.getPadding().getRight(), 0, 0));
+		}
 	}
 
 	private void onFocusChange(ObservableValue<? extends Boolean> observable, boolean oldValue, boolean newValue) {
