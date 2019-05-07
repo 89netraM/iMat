f2ba7c8d8b65b1659fbfc4566411745993558900
Robin Hallabro-Kokko <robin@hallabro.nu>
2019-05-07 16:27:30 +0200

    Add getters, setters and constructors

--- src/components/ProductItem/ProductItem.java
+++ src/components/ProductItem/ProductItem.java
@@ -1,13 +1,47 @@
 package components.ProductItem;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.VBox;
 
-public class ProductItem {
+import java.io.IOException;
+
+public class ProductItem extends VBox {
     @FXML
     private Label description;
 
     @FXML
     private ImageView image;
+
+    public ProductItem() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/components/ProductItem/ProductItem.xml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        }
+        catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    public void setDescription(Label description) {
+        this.description = description;
+    }
+
+    public void setImage(ImageView image) {
+        this.image = image;
+    }
+
+    public ImageView getImage() {
+        return image;
+    }
+
+    public Label getDescription() {
+        return description;
+    }
 }
--- src/components/ProductList/ProductList.java
+++ src/components/ProductList/ProductList.java
@@ -1,4 +1,43 @@
 package components;
 
-public class ProductList {
+import components.ProductItem.ProductItem;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ProductList extends VBox {
+    private ArrayList<ProductItem> productItems;
+
+    public ProductList() {
+        this.productItems = new ArrayList<ProductItem>();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/components/ProductList/ProductList.xml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        }
+        catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    public ArrayList<ProductItem> getProductItems() {
+        return productItems;
+    }
+
+    public void addProductItem(ProductItem productItem) {
+        this.productItems.add(productItem);
+    }
+
+    public void removeProductItem(ProductItem productItem) {
+        this.productItems.remove(productItem);
+    }
+
+    public void clearProductItems(){
+        this.productItems.clear();
+    }
 }
