e1e8e19afc6de925330d89e92b9f5a5b0d494ff4
Mårten Åsberg <marten.asberg@outlook.com>
2019-05-20 14:39:34 +0200

    Event för när en kategori väljs

--- src/CategoryCarousel/CategoryCarouselComponent.java
+++ src/CategoryCarousel/CategoryCarouselComponent.java
@@ -1,5 +1,8 @@
 package CategoryCarousel;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ScrollPane;
 import se.chalmers.cse.dat216.project.ProductCategory;
@@ -7,6 +10,8 @@ import se.chalmers.cse.dat216.project.ProductCategory;
 import java.io.IOException;
 
 public class CategoryCarouselComponent extends ScrollPane {
+	private EventHandler<CategoryCarouselComponentEvent> onSelectHandler;
+
 	private ProductCategory selectedCategory = ProductCategory.BREAD;
 
 	public CategoryCarouselComponent() {
@@ -21,11 +26,42 @@ public class CategoryCarouselComponent extends ScrollPane {
 			throw new RuntimeException(ex);
 		}
 	}
+
 	public void setSelectedCategory(ProductCategory selectedCategory) {
 		this.selectedCategory = selectedCategory;
+
+		Event event = new CategoryCarouselComponentEvent(this, selectedCategory, CategoryCarouselComponentEvent.ON_SELECT);
+		fireEvent(event);
 	}
 	public ProductCategory getSelectedCategory() {
 		return selectedCategory;
 	}
 
+	public void setOnSelect(EventHandler<CategoryCarouselComponentEvent> onSelectHandler) {
+		if (this.onSelectHandler != null) {
+			removeEventHandler(CategoryCarouselComponentEvent.ON_SELECT, this.onSelectHandler);
+		}
+
+		this.onSelectHandler = onSelectHandler;
+		addEventHandler(CategoryCarouselComponentEvent.ON_SELECT, this.onSelectHandler);
+	}
+	public EventHandler<CategoryCarouselComponentEvent> getOnSelect() {
+		return onSelectHandler;
+	}
+
+	public static class CategoryCarouselComponentEvent extends Event {
+		public static final EventType<CategoryCarouselComponentEvent> ROOT_EVENT = new EventType<>(Event.ANY, "CATEGORYCAROUSELCOMPONENT_ROOT_EVENT");
+		public static final EventType<CategoryCarouselComponentEvent> ON_SELECT = new EventType<>(ROOT_EVENT, "ON_SELECT");
+
+		private final ProductCategory selectedCategory;
+
+		public CategoryCarouselComponentEvent(CategoryCarouselComponent source, ProductCategory selectedCategory, EventType<CategoryCarouselComponentEvent> eventType) {
+			super(source, null, eventType);
+			this.selectedCategory = selectedCategory;
+		}
+
+		public ProductCategory getSelectedCategory() {
+			return selectedCategory;
+		}
+	}
 }
