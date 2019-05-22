d3da26abe389359aaaffb957743198bcd6bcb407
Mårten Åsberg <marten.asberg@outlook.com>
2019-05-22 17:28:56 +0200

    Ikoner för kategorier

--- src/CategoryCarousel/CategoryCarouselItemComponent.java
+++ src/CategoryCarousel/CategoryCarouselItemComponent.java
@@ -3,13 +3,20 @@ package CategoryCarousel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.ProductCategory;
 
+import java.io.File;
 import java.io.IOException;
 
 public class CategoryCarouselItemComponent extends GridPane {
 	private static final String SelectedClass = "selected";
+
+	@FXML
+	private ImageView image;
 
 	@FXML
 	private Label label;
@@ -32,6 +39,10 @@ public class CategoryCarouselItemComponent extends GridPane {
 
 		this.category = category;
 		label.setText(category.toString());
+
+		File file = new File(IMatDataHandler.getInstance().imatDirectory() + "/category_icons/" + category + ".png");
+		Image imageSrc = new Image(file.toURI().toString(), 100.0d, 100.0d, true, true, true);
+		image.setImage(imageSrc);
 	}
 
 	public void setIsSelected(boolean selected) {
