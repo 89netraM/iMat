90423a7e230ae734caef2a1b58cd4f124426da29
Robin Hallabro-Kokko <robin@hallabro.nu>
2019-05-22 17:52:25 +0200

    Add placeholder mainwindow

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,3 +1,29 @@
-public class MainWindow {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindow implements Initializable {
+
+    @FXML
+    private Pane searchPane;
+
+    @FXML
+    private Pane productsPane;
+
+    @FXML
+    private Pane receiptPane;
+
+    @FXML
+    private Pane paymentPane;
+
+    @FXML
+    private Pane deliveryPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
